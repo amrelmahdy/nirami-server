124750fb412a04ed9e08d41dd6a2bedf6168869e
Amr El Mahdy <amrelmhdy@hotmail.com>
2025-08-01 15:23:41 +0300

    fix build issue

--- src/modules/tickets/schemas/ticket.schema.ts
+++ src/modules/tickets/schemas/ticket.schema.ts
@@ -68,7 +68,7 @@ export class Ticket {
             'completed',
             'closed',
             'returned',
-        ], default: 'pending'
+        ], default: 'created'
     })
     status: TicketStatus;
 

1788dbf99c4384e0b2f28598bba8a0cddc9abc03
Amr El Mahdy <amrelmhdy@hotmail.com>
2025-08-18 12:22:48 +0300

    delete image when delete category issue

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -3,9 +3,13 @@ import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { categorySchema } from './schemas/category.schema';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Category', schema: categorySchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: categorySchema }]),
+    CloudinaryModule
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
